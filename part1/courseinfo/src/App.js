dfe1e6853634b866f2a3ca62923e7bd42224fd73
0bCdian <diegomiguelp1996@gmail.com>
2023-01-16 11:38:19 -0300

    1.6: unicafe step1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,52 +1,38 @@
-const Header = ({ title }) => {
-    return <h1>{title}</h1>;
-};
-const Part = (props) => (
-    <p>
-        {props.part} {props.numberOf}
-    </p>
-);
+import { useState } from "react";
 
-const Content = (props) => {
-    const part1 = props.parts[0].name;
-    const part2 = props.parts[1].name;
-    const part3 = props.parts[2].name;
-    const exercises1 = props.parts[0].exercises;
-    const exercises2 = props.parts[1].exercises;
-    const exercises3 = props.parts[2].exercises;
-    return (
-        <>
-            <Part part={part1} numberOf={exercises1} />
-            <Part part={part2} numberOf={exercises2} />
-            <Part part={part3} numberOf={exercises3} />
-        </>
-    );
-};
-const Total = (props) => {
-    const total =
-        props.parts[0].exercises +
-        props.parts[1].exercises +
-        props.parts[2].exercises;
-    return <p>Number of exercises {total} </p>;
-};
+const Heading = ({ text }) => <h2>{text}</h2>
+
+const Button = ({ action, text }) => <button onClick={action}>{text}</button>
+
+const Counters = ({ text, counter }) => <p>{text} {counter}</p>
 
 const App = () => {
-    const course = {
-        name: "Half Stack application development",
-        parts: [
-            { name: "Fundamentals of React", exercises: 10 },
-            { name: "Using props to pass data", exercises: 7 },
-            { name: "State of a component", exercises: 14 },
-        ],
-    };
+    const [feedbackValues, setFeedback] = useState({
+        good: 0,
+        neutral: 0,
+        bad: 0
+    })
+
+    const changeFeedback = (value) => {
+        const newFeedback = {
+            ...feedbackValues,
+        }
+        newFeedback[value] += 1
+        return () => setFeedback(newFeedback)
+    }
 
     return (
-        <div>
-            <Header title={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
-        </div>
-    );
-};
+        <>
+            <Heading text="Give Feedback" />
+            <Button action={changeFeedback("good")} text="good" />
+            <Button action={changeFeedback("neutral")} text="neutral" />
+            <Button action={changeFeedback("bad")} text="bad" />
+            <Heading text="Statistics" />
+            <Counters text="good: " counter={feedbackValues.good} />
+            <Counters text="neutral: " counter={feedbackValues.neutral} />
+            <Counters text="bad: " counter={feedbackValues.bad} />
+        </>
+    )
+}
 
 export default App;
